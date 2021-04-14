14f0fff5588d1b573d042c2cbf35a045090abdb4
GABRIEL ANGELO DELA CRUZ <gabdc001@gmail.com>
2021-04-14 13:38:05 +0800

    first commit

---
+++ resources/views/connect.blade.php
@@ -0,0 +1,71 @@
+<!doctype html>
+<html lang="en">
+
+    <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>BAMBOO WEBSITE</title>
+
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <link rel="icon" href="favicon.ico" type="image/x-icon">
+
+    <link rel="stylesheet" href="styles/tailwind.css">
+
+
+    </head>
+
+<body>
+
+<x-guest-layout>
+    <div class="relative w-screen h-screen bg-no-repeat bg-cover sm:bg-left-bottom" style="background-image: url('connect.png');">
+        <div class="absolute flex flex-col items-start justify-center w-full h-full">
+
+    <div class="container-pl-21 mx-20 grid grid-cols-1 pt-2 gap-5">
+       <div class="bg-white w-full flex rounded-xl shadow border p-2 transform hover:scale-105 z-10">
+        <div class="flex items-start">
+            <img src="facebook.jpg" class="w-12 h-12 ml-2 rounded">
+         </div>
+        <div class="flex-grow p-3 text-start">
+        <div class="font-semibold text-gray-800">
+        <a class="font-bold border-none no-underline focus:outline-none" href="https://www.facebook.com/bamboomusiclive/" target="_blank">FACEBOOK</a>
+        </div>
+     </div>
+</div>
+
+<div class="bg-white w-full flex items-center rounded-xl shadow border p-2 transform hover:scale-105 z-10">
+    <div class="flex items-start">
+      <img src="twitter.png" class="w-12 h-12 ml-2 rounded">
+    </div>
+    <div class="flex-grow p-3 text-start">
+      <div class="font-semibold text-gray-800">
+        <a class="font-bold border-none no-underline focus:outline-none" href="https://twitter.com/Bamboomusiclive" target="_blank">TWITTER</a>
+      </div>
+    </div>
+</div>
+
+<div class="bg-white w-full flex items-center rounded-xl shadow border p-2 transform hover:scale-105 z-10">
+    <div class="flex items-start">
+      <img src="insta.jpg" class="w-12 h-12 ml-2 rounded">
+    </div>
+    <div class="flex-grow p-3 text-start">
+      <div class="font-semibold text-gray-800">
+        <a class="font-bold border-none no-underline focus:outline-none" href="https://www.instagram.com/bamboomuzaklive" target="_blank">INSTAGRAM</a>
+      </div>
+    </div>
+</div>
+
+<div class="bg-white w-full flex items-center rounded-xl shadow border p-2 transform hover:scale-105 z-10">
+    <div class="flex items-start">
+      <img src="mail.png" class="w-12 h-12 ml-2 rounded">
+    </div>
+    <div class="flex-grow p-3 text-start">
+      <div class="font-semibold text-gray-800">
+        <a class="font-bold border-none no-underline focus:outline-none" href="" target="_blank">E-MAIL</a>
+      </div>
+    </div>
+</div>
+
+    </div>
+  </div>
+</x-guest-layout>
---
+++ resources/views/landing.blade.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="en">
+
+    <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>BAMBOO WEBSITE</title>
+
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <link rel="icon" href="favicon.ico" type="image/x-icon">
+
+    <link rel="stylesheet" href="styles/tailwind.css">
+
+
+    </head>
+
+    <body>
+    <x-guest-layout>
+        <div class="relative w-screen h-screen bg-no-repeat bg-cover sm:bg-left-bottom" style="background-image: url('artboard.png');">
+            <div class="absolute flex flex-col items-center justify-center w-full h-full justify-items-center">
+                <div class="grid grid-cols-1 gap-6">
+                    <div class="text-center justify-items-center">
+                        <a class="ml-0 text-4xl font-bold no-underline border-none lg:text-5xl hover:text-red-700 focus:outline-none active:text-red-700 hover:animate-bounce" href="store" target="_blank">STORE</a>
+                    </div>
+                    <div class="text-center justify-items-center">
+                        <a class="ml-0 text-4xl font-bold no-underline border-none lg:text-5xl hover:text-red-700 focus:outline-none active:text-red-700 hover:animate-bounce" href="listen" target="_blank">LISTEN</a>
+                    </div>
+                    <div class="text-center justify-items-center">
+                        <a class="ml-0 text-4xl font-bold no-underline border-none lg:text-5xl hover:text-red-700 focus:outline-none active:text-red-700 hover:animate-bounce" href="https://www.youtube.com/user/BAMBOOMUSICLIVE" target="_blank">WATCH</a>
+                    </div>
+                    <div class="text-center justify-items-center">
+                        <a class="ml-0 text-4xl font-bold no-underline border-none lg:text-5xl hover:text-red-700 focus:outline-none active:text-red-700 hover:animate-bounce" href="connect" target="_blank">CONNECT</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </x-guest-layout>
+    </body>
+</html>

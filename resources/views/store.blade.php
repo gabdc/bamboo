4cc1baa5ace18780d1c3c5cb71d1ac28e05d9e2a
GABRIEL ANGELO DELA CRUZ <gabdc001@gmail.com>
2021-04-14 16:55:49 +0800

    store update

--- resources/views/store.blade.php
+++ resources/views/store.blade.php
@@ -1,13 +1,150 @@
 <x-guest-layout>
-    <div class="pt-4 bg-gray-100">
-        <div class="min-h-screen flex flex-col items-center pt-6 sm:pt-0">
-            <div>
-                <x-jet-authentication-card-logo />
+<div class="container flex flex-wrap items-center justify-between w-full px-6 py-3 mx-auto mt-0">
+
+            <label for="menu-toggle" class="block cursor-pointer md:hidden">
+                <svg class="text-gray-900 fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20">
+                    <title>menu</title>
+                    <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path>
+                </svg>
+            </label>
+            <input class="hidden" type="checkbox" id="menu-toggle" />
+
+            <div class="order-3 hidden w-full md:flex md:items-center md:w-auto md:order-1" id="menu">
+                <nav>
+                    <ul class="items-center justify-between pt-4 text-base text-gray-700 md:flex md:pt-0">
+                    <a class="text-xl font-bold tracking-wide text-gray-800 no-underline uppercase hover:no-underline " href="#">Store</a>
+                    </ul>
+                </nav>
             </div>
 
-            <div class="w-full sm:max-w-2xl mt-6 p-6 bg-white shadow-md overflow-hidden sm:rounded-lg prose">
-                {!! $policy !!}
+            <div class="order-1 md:order-2">
+                <a class="flex items-center text-xl font-bold tracking-wide text-gray-800 no-underline hover:no-underline " href="#">
+                    <svg class="mr-2 text-gray-800 fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                        <path d="M5,22h14c1.103,0,2-0.897,2-2V9c0-0.553-0.447-1-1-1h-3V7c0-2.757-2.243-5-5-5S7,4.243,7,7v1H4C3.447,8,3,8.447,3,9v11 C3,21.103,3.897,22,5,22z M9,7c0-1.654,1.346-3,3-3s3,1.346,3,3v1H9V7z M5,10h2v2h2v-2h6v2h2v-2h2l0.002,10H5V10z" />
+                    </svg>
+                    BAMBOO
+                </a>
+            </div>
+
+            <div class="flex items-center order-2 md:order-3" id="nav-content">
+                <a class="inline-block no-underline hover:text-black" href="#">
+                    <svg class="fill-current hover:text-black" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"> 
+                        <path d="M12 2C9.243 2 7 4.243 7 7s2.243 5 5 5 5-2.243 5-5S14.757 2 12 2zM12 10c-1.654 0-3-1.346-3-3s1.346-3 3-3 3 1.346 3 3S13.654 10 12 10zM21 21v-1c0-3.859-3.141-7-7-7h-4c-3.86 0-7 3.141-7 7v1h2v-1c0-2.757 2.243-5 5-5h4c2.757 0 5 2.243 5 5v1H21z" />
+                    </svg>
+                </a>
+
+                <a class="inline-block pl-3 no-underline hover:text-black" href="#">
+                    <svg class="fill-current hover:text-black" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                        <path d="M21,7H7.462L5.91,3.586C5.748,3.229,5.392,3,5,3H2v2h2.356L9.09,15.414C9.252,15.771,9.608,16,10,16h8 c0.4,0,0.762-0.238,0.919-0.606l3-7c0.133-0.309,0.101-0.663-0.084-0.944C21.649,7.169,21.336,7,21,7z M17.341,14h-6.697L8.371,9 h11.112L17.341,14z" />
+                    </svg>
+                </a>
+
             </div>
         </div>
-    </div>
+    </nav>
+    <div class="container relative mx-auto carousel" style="max-width:1600px;">
+        <div class="relative w-full overflow-hidden carousel-inner">
+
+    <section class="py-8" style="background-image: url('bg1.jpeg')">
+
+        <div class="container flex flex-wrap items-center pt-4 pb-12 mx-auto">
+
+            <nav id="store" class="top-0 z-30 w-full px-6 py-1">
+                <div class="container flex flex-wrap items-center justify-between w-full px-2 py-3 mx-auto mt-0">
+
+                    <div class="flex items-center" id="store-nav-content">
+
+                        <a class="inline-block pl-3 no-underline hover:text-black" href="#">
+                            <svg class="fill-current hover:text-black" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                                <path d="M7 11H17V13H7zM4 7H20V9H4zM10 15H14V17H10z" />
+                            </svg>
+                        </a>
+
+                        <a class="inline-block pl-3 no-underline hover:text-black" href="#">
+                            <svg class="fill-current hover:text-black" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                                <path d="M10,18c1.846,0,3.543-0.635,4.897-1.688l4.396,4.396l1.414-1.414l-4.396-4.396C17.365,13.543,18,11.846,18,10 c0-4.411-3.589-8-8-8s-8,3.589-8,8S5.589,18,10,18z M10,4c3.309,0,6,2.691,6,6s-2.691,6-6,6s-6-2.691-6-6S6.691,4,10,4z" />
+                            </svg>
+                        </a>
+
+                    </div>
+              </div>
+            </nav>
+
+            <div class="flex flex-col w-full p-6 md:w-1/3 xl:w-1/4">
+                <a href="#">
+                    <img class="hover:grow hover:shadow-lg" src="https://www.dadhatlife.com/wp-content/uploads/2018/09/3179-1240xl.jpg">
+                    <div class="flex items-center justify-between pt-3">
+                        <p class="">Bamboo Music Live Dad Hat</p>
+                        <svg class="w-6 h-6 text-gray-500 fill-current hover:text-black" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                            <path d="M12,4.595c-1.104-1.006-2.512-1.558-3.996-1.558c-1.578,0-3.072,0.623-4.213,1.758c-2.353,2.363-2.352,6.059,0.002,8.412 l7.332,7.332c0.17,0.299,0.498,0.492,0.875,0.492c0.322,0,0.609-0.163,0.792-0.409l7.415-7.415 c2.354-2.354,2.354-6.049-0.002-8.416c-1.137-1.131-2.631-1.754-4.209-1.754C14.513,3.037,13.104,3.589,12,4.595z M18.791,6.205 c1.563,1.571,1.564,4.025,0.002,5.588L12,18.586l-6.793-6.793C3.645,10.23,3.646,7.776,5.205,6.209 c0.76-0.756,1.754-1.172,2.799-1.172s2.035,0.416,2.789,1.17l0.5,0.5c0.391,0.391,1.023,0.391,1.414,0l0.5-0.5 C14.719,4.698,17.281,4.702,18.791,6.205z" />
+                        </svg>
+                    </div>
+                    <p class="pt-1 text-gray-900">₱499.99</p>
+                </a>
+            </div>
+
+            <div class="flex flex-col w-full p-6 md:w-1/3 xl:w-1/4">
+                <a href="#">
+                    <img class="hover:grow hover:shadow-lg" src="https://i.pinimg.com/originals/1d/43/2a/1d432ae58f1d67c07a72e6d33775b5af.png">
+                    <div class="flex items-center justify-between pt-3">
+                        <p class="">Bamboo Music Live T-shirt</p>
+                        <svg class="w-6 h-6 text-gray-500 fill-current hover:text-black" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                            <path d="M12,4.595c-1.104-1.006-2.512-1.558-3.996-1.558c-1.578,0-3.072,0.623-4.213,1.758c-2.353,2.363-2.352,6.059,0.002,8.412 l7.332,7.332c0.17,0.299,0.498,0.492,0.875,0.492c0.322,0,0.609-0.163,0.792-0.409l7.415-7.415 c2.354-2.354,2.354-6.049-0.002-8.416c-1.137-1.131-2.631-1.754-4.209-1.754C14.513,3.037,13.104,3.589,12,4.595z M18.791,6.205 c1.563,1.571,1.564,4.025,0.002,5.588L12,18.586l-6.793-6.793C3.645,10.23,3.646,7.776,5.205,6.209 c0.76-0.756,1.754-1.172,2.799-1.172s2.035,0.416,2.789,1.17l0.5,0.5c0.391,0.391,1.023,0.391,1.414,0l0.5-0.5 C14.719,4.698,17.281,4.702,18.791,6.205z" />
+                        </svg>
+                    </div>
+                    <p class="pt-1 text-gray-900">₱699.99</p>
+                </a>
+            </div>
+
+            <div class="flex flex-col w-full p-6 md:w-1/3 xl:w-1/4">
+                <a href="#">
+                    <img class="hover:grow hover:shadow-lg" src="https://cdn.shopify.com/s/files/1/1368/3463/products/BLACK-HOODIE_df8f1683-ff82-4893-a681-0fd3620ec894.jpg?v=1596732085">
+                    <div class="flex items-center justify-between pt-3">
+                        <p class="">Bamboo Music Live Hoodie</p>
+                        <svg class="w-6 h-6 text-gray-500 fill-current hover:text-black" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                            <path d="M12,4.595c-1.104-1.006-2.512-1.558-3.996-1.558c-1.578,0-3.072,0.623-4.213,1.758c-2.353,2.363-2.352,6.059,0.002,8.412 l7.332,7.332c0.17,0.299,0.498,0.492,0.875,0.492c0.322,0,0.609-0.163,0.792-0.409l7.415-7.415 c2.354-2.354,2.354-6.049-0.002-8.416c-1.137-1.131-2.631-1.754-4.209-1.754C14.513,3.037,13.104,3.589,12,4.595z M18.791,6.205 c1.563,1.571,1.564,4.025,0.002,5.588L12,18.586l-6.793-6.793C3.645,10.23,3.646,7.776,5.205,6.209 c0.76-0.756,1.754-1.172,2.799-1.172s2.035,0.416,2.789,1.17l0.5,0.5c0.391,0.391,1.023,0.391,1.414,0l0.5-0.5 C14.719,4.698,17.281,4.702,18.791,6.205z" />
+                        </svg>
+                    </div>
+                    <p class="pt-1 text-gray-900">₱1299.99</p>
+                </a>
+            </div>
+
+            <div class="flex flex-col w-full p-6 md:w-1/3 xl:w-1/4">
+                <a href="#">
+                    <img class="hover:grow hover:shadow-lg" src="https://images.canadagoose.com/image/upload/w_1333,c_scale,f_auto,q_auto:best/v1525896890/product-image/6980M_61_d.jpg">
+                    <div class="flex items-center justify-between pt-3">
+                        <p class="">Bamboo Music Live Sweater</p>
+                        <svg class="w-6 h-6 text-gray-500 fill-current hover:text-black" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                            <path d="M12,4.595c-1.104-1.006-2.512-1.558-3.996-1.558c-1.578,0-3.072,0.623-4.213,1.758c-2.353,2.363-2.352,6.059,0.002,8.412 l7.332,7.332c0.17,0.299,0.498,0.492,0.875,0.492c0.322,0,0.609-0.163,0.792-0.409l7.415-7.415 c2.354-2.354,2.354-6.049-0.002-8.416c-1.137-1.131-2.631-1.754-4.209-1.754C14.513,3.037,13.104,3.589,12,4.595z M18.791,6.205 c1.563,1.571,1.564,4.025,0.002,5.588L12,18.586l-6.793-6.793C3.645,10.23,3.646,7.776,5.205,6.209 c0.76-0.756,1.754-1.172,2.799-1.172s2.035,0.416,2.789,1.17l0.5,0.5c0.391,0.391,1.023,0.391,1.414,0l0.5-0.5 C14.719,4.698,17.281,4.702,18.791,6.205z" />
+                        </svg>
+                    </div>
+                    <p class="pt-1 text-gray-900">₱899.99</p>
+                </a>
+            </div>
+        </div>
+    </section>
+     <footer class="container py-8 mx-auto bg-white border-t border-gray-400">
+        <div class="container flex px-3 py-8 ">
+            <div class="flex flex-wrap w-full mx-auto">
+                <div class="flex w-full lg:w-1/2 ">
+                    <div class="px-3 md:px-0">
+                        <h3 class="font-bold text-gray-900">About</h3>
+                        <p class="py-4">
+                            This is the official Bamboo Music Live online shop where in you can find the exclusive Bamboo Music Live Merchandise and to be delivered in your doorstep.
+                        </p>
+                    </div>
+                </div>
+                <div class="flex w-full lg:w-1/2 lg:justify-end lg:text-right">
+                    <div class="px-3 md:px-0">
+                        <h3 class="font-bold text-gray-900">Social</h3>
+                        <ul class="items-center pt-3 list-reset">
+                            <li><a class="inline-block py-1 no-underline hover:text-black hover:underline" href="#">INSTAGRAM</a></li>
+                            <li><a class="inline-block py-1 no-underline hover:text-black hover:underline" href="#">FACEBOOK</a></li>
+                            <li><a class="inline-block py-1 no-underline hover:text-black hover:underline" href="#">TWITTER</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
 </x-guest-layout>

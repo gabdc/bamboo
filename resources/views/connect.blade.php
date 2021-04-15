6b8779639b9a201097c800ffa8fd1ad4f9079b07
GABRIEL ANGELO DELA CRUZ <gabdc001@gmail.com>
2021-04-15 17:08:49 +0800

    adjustments of design based on client's feedback

--- resources/views/connect.blade.php
+++ resources/views/connect.blade.php
@@ -1,52 +1,52 @@
 <x-guest-layout>
-    <div class="relative w-screen h-screen bg-no-repeat bg-cover sm:bg-left-bottom" style="background-image: url('connect.png');">
-        <div class="absolute flex flex-col items-start justify-center w-full h-full">
+<div class="relative w-screen h-screen bg-right bg-no-repeat bg-cover sm:bg-left-bottom" style="background-image: url('connect.png');">
+    <div class="absolute flex flex-col items-start justify-center w-full h-full">
 
-    <div class="container-pl-21 mx-20 grid grid-cols-1 pt-2 gap-5">
-       <div class="bg-white w-full flex rounded-xl shadow border p-2 transform hover:scale-105 z-10">
-        <div class="flex items-start">
-            <img src="facebook.jpg" class="w-12 h-12 ml-2 rounded">
-         </div>
-        <div class="flex-grow p-3 text-start">
-        <div class="font-semibold text-gray-800">
-        <a class="font-bold border-none no-underline focus:outline-none" href="https://www.facebook.com/bamboomusiclive/" target="_blank">FACEBOOK</a>
+<div class="grid grid-cols-1 gap-5 pt-2 mx-20 container-pl-21">
+    <div class="z-10 flex w-full p-2 transform bg-white border shadow rounded-xl hover:scale-105">
+    <div class="flex items-start">
+        <img src="facebook.jpg" class="w-12 h-12 ml-2 rounded">
         </div>
-     </div>
-</div>
-
-<div class="bg-white w-full flex items-center rounded-xl shadow border p-2 transform hover:scale-105 z-10">
-    <div class="flex items-start">
-      <img src="twitter.png" class="w-12 h-12 ml-2 rounded">
-    </div>
     <div class="flex-grow p-3 text-start">
-      <div class="font-semibold text-gray-800">
-        <a class="font-bold border-none no-underline focus:outline-none" href="https://twitter.com/Bamboomusiclive" target="_blank">TWITTER</a>
-      </div>
+    <div class="font-semibold text-gray-800">
+    <a class="font-bold no-underline border-none focus:outline-none" href="https://www.facebook.com/bamboomusiclive/" target="_blank">FACEBOOK</a>
+    </div>
     </div>
 </div>
 
-<div class="bg-white w-full flex items-center rounded-xl shadow border p-2 transform hover:scale-105 z-10">
-    <div class="flex items-start">
-      <img src="insta.jpg" class="w-12 h-12 ml-2 rounded">
-    </div>
-    <div class="flex-grow p-3 text-start">
-      <div class="font-semibold text-gray-800">
-        <a class="font-bold border-none no-underline focus:outline-none" href="https://www.instagram.com/bamboomuzaklive" target="_blank">INSTAGRAM</a>
-      </div>
+<div class="z-10 flex items-center w-full p-2 transform bg-white border shadow rounded-xl hover:scale-105">
+<div class="flex items-start">
+    <img src="twitter.png" class="w-12 h-12 ml-2 rounded">
+</div>
+<div class="flex-grow p-3 text-start">
+    <div class="font-semibold text-gray-800">
+    <a class="font-bold no-underline border-none focus:outline-none" href="https://twitter.com/Bamboomusiclive" target="_blank">TWITTER</a>
     </div>
 </div>
-
-<div class="bg-white w-full flex items-center rounded-xl shadow border p-2 transform hover:scale-105 z-10">
-    <div class="flex items-start">
-      <img src="mail.png" class="w-12 h-12 ml-2 rounded">
-    </div>
-    <div class="flex-grow p-3 text-start">
-      <div class="font-semibold text-gray-800">
-        <a class="font-bold border-none no-underline focus:outline-none" href="" target="_blank">E-MAIL</a>
-      </div>
-    </div>
 </div>
 
+<div class="z-10 flex items-center w-full p-2 transform bg-white border shadow rounded-xl hover:scale-105">
+<div class="flex items-start">
+    <img src="insta.jpg" class="w-12 h-12 ml-2 rounded">
+</div>
+<div class="flex-grow p-3 text-start">
+    <div class="font-semibold text-gray-800">
+    <a class="font-bold no-underline border-none focus:outline-none" href="https://www.instagram.com/bamboomuzaklive" target="_blank">INSTAGRAM</a>
     </div>
-  </div>
+</div>
+</div>
+
+<div class="z-10 flex items-center w-full p-2 transform bg-white border shadow rounded-xl hover:scale-105">
+<div class="flex items-start">
+    <img src="mail.png" class="w-12 h-12 ml-2 rounded">
+</div>
+<div class="flex-grow p-3 text-start">
+    <div class="font-semibold text-gray-800">
+    <a class="font-bold no-underline border-none focus:outline-none" href="" target="_blank">E-MAIL</a>
+    </div>
+</div>
+</div>
+
+</div>
+</div>
 </x-guest-layout>
--- resources/views/listen.blade.php
+++ resources/views/listen.blade.php
@@ -1,67 +1,111 @@
 
-    <x-guest-layout>
-        <div class="w-screen h-screen flex justify-start items-center bg-cover" style="background-image: url('listenbg.png');">
-            
-         <div class="container-pl-21 mx-20 max-w-xs rounded-lg overflow-hidden shadow-xl my-2 bg-gray-900 border border-gray-900">
-            <div class="relative">
-               <img class="w-full" src="tape.png"/>
-            </div>
-            <div class="px-6 py-4 text-center tracking-wide6">
-                <div class="mb-6">
-        
-                    <!-- <p class="text-white tracking-wide uppercase text-lg font-extrabold">BAMBOO</p>
-                    <p class="text-gray-200 text-sm">@bamboomuzaklive</p> -->
+<x-guest-layout>
+    <div class="hidden md:block">
+        <div class="flex items-center justify-center w-screen h-screen bg-cover" style="background-image: url('listenbgct.jpg');">
+                <!-- <p class="text-lg font-extrabold tracking-wide text-white uppercase">BAMBOO</p>
+                <p class="text-sm text-gray-200">@bamboomuzaklive</p> -->
+            <div class="grid grid-cols-4 gap-2 pt-40">
 
-                    <div class="grid grid-cols-1 pt-2 gap-2">
-
-                        <div class="bg-white w-full flex items-center rounded-xl shadow border p-2 transform hover:scale-105 z-10">
-                            <div class="flex items-start">
-                              <img src="applemusic.png" class="w-12 h-12 ml-2 rounded">
-                            </div>
-                            <div class="flex-grow p-3 text-start">
-                              <div class="font-semibold text-gray-800">
-                                <a class="font-bold border-none no-underline focus:outline-none" href="https://music.apple.com/us/album/bless-this-mess/1050936498" target="_blank">APPLE MUSIC</a>
-                              </div>
-                            </div>
-                        </div>
-
-                        <div class="bg-white w-full flex items-center rounded-xl shadow border p-2 transform hover:scale-105 z-10">
-                            <div class="flex items-start">
-                              <img src="spotify.png" class="w-12 h-12 ml-2 rounded">
-                            </div>
-                            <div class="flex-grow p-3 text-start">
-                              <div class="font-semibold text-gray-800">
-                                <a class="font-bold border-none no-underline focus:outline-none" href="https://open.spotify.com/album/6FcyHSrekDFm9SYZnsyoQ8?si=HXikfNf4TBGn6-_iIOnK2w" target="_blank">SPOTIFY MUSIC</a>
-                              </div>
-                            </div>
-                        </div>
-
-                        <div class="bg-white w-full flex items-center rounded-xl shadow border p-2 transform hover:scale-105 z-10">
-                            <div class="flex items-start">
-                              <img src="youtube music.png" class="w-12 h-12 ml-2 rounded">
-                            </div>
-                            <div class="flex-grow p-3 text-start">
-                              <div class="font-semibold text-gray-800">
-                                <a class="font-bold border-none no-underline focus:outline-none" href="https://music.youtube.com/playlist?list=OLAK5uy_m19nefqd7hnYoAXBOlDus-kjjxbKjCGEw" target="_blank">YOUTUBE MUSIC</a>
-                              </div>
-                            </div>
-                        </div>
-
-                        <div class="bg-white w-full flex items-center rounded-xl shadow border p-2 transform hover:scale-105 z-10">
-                            <div class="flex items-start">
-                              <img src="amazonmusic.png" class="w-12 h-12 ml-2 rounded">
-                            </div>
-                            <div class="flex-grow p-3 text-start">
-                              <div class="font-semibold text-gray-800">
-                                <a class="font-bold border-none no-underline focus:outline-none" href="https://music.amazon.com/albums/B016WMKX24?marketplaceId=ATVPDKIKX0DER&musicTerritory=US" target="_blank">AMAZON MUSIC</a>
-                              </div>
-                            </div>
-                        </div>
-
+                <div class="z-10 flex items-center w-full p-2 transform bg-white border-4 shadow focus:outline-gray-900 rounded-xl hover:scale-105">
+                    <div class="flex items-start">
+                        <img src="applemusic.png" class="h-12 ml-2 rounded w-14">
                     </div>
+                    <div class="flex-grow p-3 text-start">
+                        <div class="font-semibold text-gray-800">
+                        <a class="font-bold no-underline border-none focus:outline-none" href="https://music.apple.com/us/album/bless-this-mess/1050936498" target="_blank">APPLE MUSIC</a>
+                        </div>
+                    </div>
+                </div>
 
-                   </div>
-               </div>
+                <div class="z-10 flex items-center w-full p-2 transform bg-white border-4 shadow focus:outline-gray-900 rounded-xl hover:scale-105">
+                    <div class="flex items-start">
+                        <img src="spotify.png" class="w-12 h-12 ml-2 rounded">
+                    </div>
+                    <div class="flex-grow p-3 text-start">
+                        <div class="font-semibold text-gray-800">
+                        <a class="font-bold no-underline border-none focus:outline-none" href="https://open.spotify.com/album/6FcyHSrekDFm9SYZnsyoQ8?si=HXikfNf4TBGn6-_iIOnK2w" target="_blank">SPOTIFY MUSIC</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="z-10 flex items-center w-full p-2 transform bg-white border-4 shadow focus:outline-gray-900 rounded-xl hover:scale-105">
+                    <div class="flex items-start">
+                        <img src="youtube music.png" class="w-12 h-12 ml-2 rounded">
+                    </div>
+                    <div class="flex-grow p-3 text-start">
+                        <div class="font-semibold text-gray-800">
+                        <a class="font-bold no-underline border-none focus:outline-none" href="https://music.youtube.com/playlist?list=OLAK5uy_m19nefqd7hnYoAXBOlDus-kjjxbKjCGEw" target="_blank">YOUTUBE MUSIC</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="z-10 flex items-center w-full p-2 transform bg-white border-4 shadow focus:outline-gray-900 rounded-xl hover:scale-105">
+                    <div class="flex items-start">
+                        <img src="amazonmusic.png" class="w-12 ml-2 rounded h-11">
+                    </div>
+                    <div class="flex-grow p-3 text-start">
+                        <div class="font-semibold text-gray-800">
+                        <a class="font-bold no-underline border-none focus:outline-none" href="https://music.amazon.com/albums/B016WMKX24?marketplaceId=ATVPDKIKX0DER&musicTerritory=US" target="_blank">AMAZON MUSIC</a>
+                        </div>
+                    </div>
+                </div>
+
             </div>
-         </div>
-      </x-guest-layout>
+
+        </div>
+    </div>
+    <div class="block md:hidden">
+        <div class="flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-gray-900 border-4 border-gray-600">
+            <div class="relative w-full h-64 py-20">
+                <img class="w-auto h-auto" src="tape.png"/>
+            </div>
+            <div class="grid grid-cols-1 gap-5 py-48">
+
+                <div class="flex items-center w-full p-2 transform bg-white border-4 shadow focus:outline-gray-900 rounded-xl hover:scale-105">
+                    <div class="flex items-start">
+                        <img src="applemusic.png" class="h-12 ml-2 rounded w-14">
+                    </div>
+                    <div class="flex-grow p-3 text-start">
+                        <div class="font-semibold text-gray-800">
+                        <a class="font-bold no-underline border-none focus:outline-none" href="https://music.apple.com/us/album/bless-this-mess/1050936498" target="_blank">APPLE MUSIC</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="flex items-center w-full p-2 transform bg-white border-4 shadow focus:outline-gray-900 rounded-xl hover:scale-105">
+                    <div class="flex items-start">
+                        <img src="spotify.png" class="w-12 h-12 ml-2 rounded">
+                    </div>
+                    <div class="flex-grow p-3 text-start">
+                        <div class="font-semibold text-gray-800">
+                        <a class="font-bold no-underline border-none focus:outline-none" href="https://open.spotify.com/album/6FcyHSrekDFm9SYZnsyoQ8?si=HXikfNf4TBGn6-_iIOnK2w" target="_blank">SPOTIFY MUSIC</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="flex items-center w-full p-2 transform bg-white border-4 shadow focus:outline-gray-900 rounded-xl hover:scale-105">
+                    <div class="flex items-start">
+                        <img src="youtube music.png" class="w-12 h-12 ml-2 rounded">
+                    </div>
+                    <div class="flex-grow p-3 text-start">
+                        <div class="font-semibold text-gray-800">
+                        <a class="font-bold no-underline border-none focus:outline-none" href="https://music.youtube.com/playlist?list=OLAK5uy_m19nefqd7hnYoAXBOlDus-kjjxbKjCGEw" target="_blank">YOUTUBE MUSIC</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="flex items-center w-full p-2 transform bg-white border-4 shadow focus:outline-gray-900 rounded-xl hover:scale-105">
+                    <div class="flex items-start">
+                        <img src="amazonmusic.png" class="w-12 ml-2 rounded h-11">
+                    </div>
+                    <div class="flex-grow p-3 text-start">
+                        <div class="font-semibold text-gray-800">
+                        <a class="font-bold no-underline border-none focus:outline-none" href="https://music.amazon.com/albums/B016WMKX24?marketplaceId=ATVPDKIKX0DER&musicTerritory=US" target="_blank">AMAZON MUSIC</a>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</x-guest-layout>
